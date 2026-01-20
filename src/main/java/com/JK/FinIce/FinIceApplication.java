6ba08795f0201a74189e5299cc2c8d3b0c01ed53
Jasurbek Khamroev <khamroevapple@gmail.com>
2026-01-20 11:42:37 +0100

    setup commit

---
+++ src/main/java/com/JK/FinIce/FinIceApplication.java
@@ -0,0 +1,13 @@
+package com.JK.FinIce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinIceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinIceApplication.class, args);
+	}
+
+}
